1bd66000801a173f4ef015b8e8d08ac433903ede
Sevdzhan Alkan <sevdzhan.alkan@sheppatechnologies.com>
2023-04-16 01:37:58 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sevcan Alkan on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
